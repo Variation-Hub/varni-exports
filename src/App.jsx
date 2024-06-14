6952e90ced781ab2ff5c3bb5c43046408e3030d8
Harsh Domadiya <harshdomadiya2@gmail.com>
2024-06-14 18:04:23 +0530

    About-us page

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,8 @@ import router from './router'
 import './index.css'
 
 function App() {
-  return ( 
-    
+  return (
     <RouterProvider router={router} />
-    
   )
 }
 
--- src/contant.jsx
+++ src/contant.jsx
@@ -1,6 +1,14 @@
 import banner1 from './assets/banner-1.png';
 import banner2 from './assets/banner-2.png';
 import banner3 from './assets/banner-3.png';
+import Black_raisins from './assets/Black_raisins.jpg';
+import Brown_Sesame from './assets/Brown_Sesame.png';
+import Cardamom from './assets/Cardamom.jpg';
+import Currants from './assets/Currants.jpg';
+import guar_meal from './assets/guar_meal.jpg';
+import Red_Raisin from './assets/Red_Raisin.jpg';
+import Split_bengal_gram_chana_dal from './assets/Split_bengal_gram_chana_dal.jpg';
+import White_Onion from './assets/White_Onion.png';
 import { IoShieldHalfSharp } from "react-icons/io5";
 import { FaHandshake } from "react-icons/fa6";
 import { FaUserClock } from "react-icons/fa";
@@ -8,7 +16,7 @@ import { FaUserClock } from "react-icons/fa";
 
 export const catagories = [
     { name: 'Rice', path: '/products/Rice' },
-    { name: 'Wheat (Whole Grain)', path: '/products/Wheat'  },
+    { name: 'Wheat (Whole Grain)', path: '/products/Wheat' },
     { name: 'Barley', path: '/products/Barley' },
     { name: 'Steel-Cut Oats', path: '/products/Oats' },
     { name: 'Quinoa', path: '/products/Quinoa' },
@@ -36,6 +44,17 @@ export const contactUsSection = {
     companyName: "BHIMANI EXPORTS PVT. LTD",
     address: "B-705, Titanium Heights, Opp. Vodafone House, Corporate Rd, Prahlad Nagar, Ahmedabad, 380015 Gujarat, India",
     contact: "+91-9033903314",
-    email:"bhimaniexports@gmail.com"
+    email: "bhimaniexports@gmail.com"
 
 }
+
+export const products = [
+    { id: 1, name: "Black-raisins", image: Black_raisins },
+    { id: 2, name: "Brown-Sesame", image: Brown_Sesame },
+    { id: 3, name: "Cardamom", image: Cardamom },
+    { id: 4, name: "Currants", image: Currants },
+    { id: 5, name: "guar-meal", image: guar_meal },
+    { id: 6, name: "Red-Raisin", image: Red_Raisin },
+    { id: 7, name: "Split-bengal-gram-chana-dal", image: Split_bengal_gram_chana_dal },
+    { id: 8, name: "White-Onion", image: White_Onion },
+]
